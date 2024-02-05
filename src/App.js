1575332c389b66e713326993a44386e4b076e4fe
sagar <kakkarsagar03@gmail.com>
2024-02-05 22:41:09 +0530

    edited Learn

--- src/App.js
+++ src/App.js
@@ -2,15 +2,15 @@ import './App.css';
 import LSideBar from './components/LSideBar.js';
 import Sections from './screens/Sections/Sections.js';
 import { BrowserRouter as Router, Routes, Route} from "react-router-dom";
-import Learn from "./screens/Sections/Learn/Learn.js"
+import Learn from "./screens/Learn/Learn.js"
 function App() {
   return (
     <Router>
     <div className="app">
       <div className="sidebar"><LSideBar /></div>
-      <Sections/>
       <Routes>
-        <Route path ='/learn' element={<Learn />} />
+      <Route path ='/learn' element={<Learn />} />
+      <Route path ='/' element={<Sections />} />
       </Routes>
     </div>
     </Router>
