161de2588be36b060530c6fe238a1754bd9e1aac
Garvit Monga <garvit661@gmail.com>
2024-02-05 23:10:46 +0530

    rsb

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import './App.css';
-import SideBar from './components/SideBar.js';
+import LSideBar from './components/LSideBar.js';
 
 import Sections from './screens/Sections/Sections.js';
 
 function App() {
   return (
     <div className="app">
-      <div className="sidebar"><SideBar /></div>
+      <div className="sidebar"><LSideBar /></div>
       <Sections/>
     </div>
   );
