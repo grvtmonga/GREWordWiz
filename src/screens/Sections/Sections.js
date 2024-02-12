2795e692f16eb75440a00a463f20fdda988ee493
Garvit Monga <garvit661@gmail.com>
2024-02-12 14:48:00 +0530

    added section icon

--- src/screens/Sections/Sections.js
+++ src/screens/Sections/Sections.js
@@ -1,26 +1,33 @@
 import React from 'react'
+import Icons from '../../components/icons'
 import SectionBlock from './sectionBlock/SectionBlock';
 import RSideBar from '../../components/RSideBar';
+import foundation from '../../Assets/images/foundation.png'
+import skillBridge from '../../Assets/images/skillBridge.png'
+import Zenith from '../../Assets/images/Zenith.png'
 import './Sections.css'
 const sects = [
     {
         title:'Foundations of Expertise',
         subTitle:'Embarking on the Learning Voyage',
+        icon : <Icons source={foundation} name="Foundation" />
     },
     {
         title:'Skillbridge',
         subTitle:'The Mastery Midpoint',
+        icon : <Icons source={skillBridge} name="SkillBridge" />
     },
     {
         title:'Zenith',
         subTitle:'The Mastery Module',
+        icon : <Icons source={Zenith} name="Zenith" />
     }
 ]   
 
 export default function Sections() {
 
     const sectItems = sects.map((val, key) => (
-        <SectionBlock key={key} title={val.title} subTitle={val.subTitle} />
+        <SectionBlock key={key} title={val.title} subTitle={val.subTitle}  icon={val.icon}/>
       ));
   return (
     <div className='sect'>
--- src/screens/Sections/sectionBlock/SectionBlock.js
+++ src/screens/Sections/sectionBlock/SectionBlock.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import './SectionBlock.css'
 import { Link } from 'react-router-dom';
-export default function SectionBlock({title,subTitle}) {
+export default function SectionBlock({title,subTitle,icon}) {
   return (
     <div className='sb'>
       <div className='details'>
         <p id="title">{title}</p>
         <p id='subTitle'>{subTitle}</p>
+        <img src='{icon}'/>
         <div className='progress'></div>
         <Link to="/learn">
           <button id='sect-button'>Start</button>
