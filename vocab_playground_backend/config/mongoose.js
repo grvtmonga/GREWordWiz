1704cbca44d6705a8d1f03173423954c24109254
Garvit Monga <garvit661@gmail.com>
2024-02-12 08:03:59 +0530

    inititated backend

---
+++ vocab_playground_backend/config/mongoose.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+
+const mongoURL = 'mongodb+srv://vocab_playground:Vocab123@cluster1.txpwnvz.mongodb.net/vocab_playground';
+
+const mongoDb = async () => {
+    try {
+        await mongoose.connect(mongoURL, {
+            useNewUrlParser: true,
+        });
+        console.log('Database is connected');
+    } catch (error) {
+        console.error('Error connecting to the database:', error.message);
+    }
+};
+module.exports = mongoDb;
