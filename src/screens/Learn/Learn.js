7c686d353bdb748d8366ad01b69f69f9fbd602d6
sagar <kakkarsagar03@gmail.com>
2024-02-12 10:40:24 +0530

    MergedLastOneWasError

--- src/screens/Learn/Learn.js
+++ src/screens/Learn/Learn.js
@@ -1,19 +1,21 @@
 import React from "react";
 import "./Learn.css"
 import { useNavigate } from "react-router-dom";
+import RSideBar from "../../components/RSideBar"
 
 function Learn() {
   const navigate = useNavigate();
 
   return (
-    <div className="learn">
+    <div className='learn'>
 
       <div class="learn-header">
       <button 
-          type="button" onClick={() => navigate('/')} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" viewBox="0 0 16 16">
-          <path fill-rule="evenodd" d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
+          type="button" className="buttom-top-learn" onClick={() => navigate('/')} ><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-arrow-left" >
+          <path d="M15 8a.5.5 0 0 0-.5-.5H2.707l3.147-3.146a.5.5 0 1 0-.708-.708l-4 4a.5.5 0 0 0 0 .708l4 4a.5.5 0 0 0 .708-.708L2.707 8.5H14.5A.5.5 0 0 0 15 8"/>
         </svg></button>
         </div>
+      <div className="learn-mid">
         <div>
         <button 
           type="button" 
@@ -22,8 +24,11 @@ function Learn() {
           >
         </button>
       </div>
-      <div>
       </div>
+      <div className='rsb-learn'>
+            <RSideBar/>
+        </div>
+     
     </div>
   );
 }
