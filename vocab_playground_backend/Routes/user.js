694a31dca62140837ae9c03fec4f5c9125e20a90
sagar <kakkarsagar03@gmail.com>
2024-02-13 11:50:39 +0530

    Updated

---
+++ vocab_playground_backend/Routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/User')
+
+router.post('/createUser', async (req, res)=>{
+    try{
+
+    } catch(error){
+        
+    }
+
+})
--- vocab_playground_backend/index.js
+++ vocab_playground_backend/index.js
@@ -7,7 +7,7 @@ mongoDb()
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
-
+app.user()
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
